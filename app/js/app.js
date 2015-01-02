3981040ff4ecdf6ab2a82e510260b173783ba96a
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-01-02 18:05:30 +0200

    added routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,15 @@
 'use strict'
 
-var adsModule = angular.module('adsModule', []);
+var adsModule = angular.module('adsModule', ['ngRoute']);
+
+adsModule.config(function($routeProvider){
+	$routeProvider
+		.when(
+			'/login',
+			{ templateUrl:'templates/login.html' }
+			)
+		.when(
+			'/register',
+			{ templateUrl:'templates/register.html' }
+			)
+});
