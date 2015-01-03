f94e3239701d94a4ac5d0c50bb6467fc47c46f9c
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-01-03 23:47:11 +0200

    added controller functionality for filters

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,13 +1,52 @@
-adsModule.controller('MainController', function($scope, mainData, constants){
+adsModule.controller('MainController', function($scope, mainData, constants,$log){
+	$scope.constants = constants;
+
 	mainData.getAllAds(
 		function success(data, status, headers, config){
 			$scope.ads = data.ads;
-			$scope.constants = constants;
-			$scope.data = data;
 		}, 
-		function error(){
+		function error(data, status, headers, config){
 			$log.error(data);
 		},
 		2
-	)
+	);
+
+	mainData.getAllTowns(
+		function success(data, status, headers, config){
+			data.unshift({ id: 0, name: "All", class: "active"});
+			$scope.towns = data;
+			$scope.selectedTown = 0;
+		},
+		function error(data, status, headers, config){
+			$log.error(data);
+		}
+	);
+
+	mainData.getAllCategories(
+		function success(data, status, headers, config){
+			data.unshift({ id: 0, name: "All"});
+			$scope.categories = data;
+			$scope.selectedCategory = 0;
+		},
+		function error(data, status, headers, config){
+			$log.error(data);
+		}
+	);
+
+	function onFilterClicked(townId, categoryId){
+		mainData.getAdsWithFilters(
+			function success(data, status, headers, config){
+				$scope.ads = data.ads;
+				$scope.selectedTown = townId;
+				$scope.selectedCategory = categoryId;
+			}, 
+			function error(data, status, headers, config){
+				$log.error(data);
+			},
+			townId, 
+			categoryId
+		);
+	}
+
+	$scope.onFilterClicked = onFilterClicked;
 });
