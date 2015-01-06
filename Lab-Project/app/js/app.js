5c74b9e9a34ee1d9234aed6111134515491929ad
Tsvetina Georgieva <tsv.georgieva@abv.bg>
2015-01-06 20:56:53 +0200

    registration

--- Lab-Project/app/js/app.js
+++ Lab-Project/app/js/app.js
@@ -21,9 +21,24 @@ app.config(function ($routeProvider) {
         templateUrl: 'templates/register.html',
         controller: 'RegisterController'
     });
-    
+
+    $routeProvider.when('/user/ads/publish', {
+        templateUrl: 'templates/user/publish-new-ad.html',
+        controller: 'UserPublishNewAdController'
+    });
+
     $routeProvider.otherwise(
         { redirectTo: '/' }
     );
 
 });
+
+app.run(function ($rootScope, $location, authService) {
+  $rootScope.$on('$locationChangeStart', function (event) {
+    if ($location.path().indexOf("/user/") != -1 && !authService.isLoggedIn()) {
+      // Authorization check: anonymous site visitors cannot access user routes
+      $location.path("/");
+    }
+  });
+});
+
--- Lab-Project/app/js/controllers/RegisterController.js
+++ Lab-Project/app/js/controllers/RegisterController.js
@@ -1,8 +1,21 @@
 'use strict';
 
-// The RegisterController is responsible for the "Register" screen
 app.controller('RegisterController',
-    function ($scope, $location, authService, notifyService) {
-        // TODO
+    function ($scope, $location, townsService, authService, notifyService) {
+        $scope.userData = {townId: null};
+
+        $scope.towns = townsService.getTowns();
+
+        $scope.register = function(userData) {
+            authService.register(userData,
+                function success() {
+                    notifyService.showInfo('Registration successful');
+            		$location.path('/');
+                },
+                function error(err) {
+                    notifyService.showError("User registration failed", err);
+                }
+            );
+        };
     }
 );
